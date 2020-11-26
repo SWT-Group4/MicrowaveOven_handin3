405e5076fff41ac3aee0d67c1b72b6e1f4c32bd7
Malthe Brauer-Nielsen <au608380@uni.au.dk>
2020-11-26 09:01:50 +0100

    Added IT2 Implementation, needs to be checked manual

---
+++ Microwave.Test.Integration/IT2_Display.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+//using NSubstitute;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT2_Display
+    {
+        private IDisplay _display;
+        private IOutput _output;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = new Output();
+            _display = new Display(_output);
+        }
+
+        [Test]
+        public void OnShowTime_OutputWith42MinAnd42Sec_IsMade()
+        {
+            _display.ShowTime(42, 42);
+        }
+
+        [Test]
+        public void OnShowPower_OutputOf42W_IsMade()
+        {
+            _display.ShowPower(42);
+        }
+
+        [Test]
+        public void OnClear_OutputMessage_WithClearedIsMade()
+        {
+            _display.Clear();
+        }
+    }
+}
